b2d885cf7a22c28ca46c6543816577f6d88f6815
Nelson Chen <crazysim@gmail.com>
2019-06-18 07:38:39 -0700

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ rm archive.zip
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-git status
 git add -A
 git commit --all -m "Archive Updated on $timestamp"
-git push https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git master --force
+git status
+git push https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git HEAD:master

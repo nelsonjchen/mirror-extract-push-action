ba7f0a9df7c33e2d050d6d87fdb545a244a631ef
Nelson Chen <crazysim@gmail.com>
2019-06-25 23:35:23 -0700

    If there's nothing to commit, exit with neutral status

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 git add -A
-git commit --all -m "Archive Updated on $timestamp"
+# If there's nothing to commit, exit with neutral status
+git commit --all -m "Archive Updated on $timestamp" || exit 78
 git status
 git push https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git HEAD:master
